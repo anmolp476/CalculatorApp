416cae62e763e3d206b687a82ff59b589b0038e9
Anmol <anmolp476@gmail.com>
2022-08-29 19:01:16 -0400

    Created the add(event) and the equals() function for testing

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
+let result = 0;
+let firstNum = 0;
+let secondNum = 0;
+
 function number(event)
 {
     const output = document.getElementById("output");
     const button = event.target;
     output.value = button.innerHTML;
+    firstNum = +button.innerHTML;
+    console.log(button.id);
 }
+
+const add = (event) =>
+{
+    const output = document.getElementById("output");
+    const buttonPressedId = event.target.id;
+
+    if(result == 0)
+    {
+        output.value = "Please enter a number";
+        return;
+    } 
+}
+
+
+const equals = (string) =>
+{
+    
+}
+
+
